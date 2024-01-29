0d3d8a8abf4e5b19f510b0e1df7d2e89158b3aa7
Vincent Liu <Vincent00lav@outlook.com>
2024-01-29 10:59:56 +0100

    Content

---
+++ main.js
@@ -0,0 +1,36 @@
+const generateButton = document.getElementById("generateButton");
+const copyButton = document.getElementById("copyButton");
+const show = document.querySelector(".show");
+const passwordInput = document.getElementById("password");
+const pwdLengthSelect = document.getElementById("pwdLengthSelect");
+
+generateButton.onclick = function generatePassword() {
+  const character =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_";
+  const pwdLength = pwdLengthSelect.value;
+  let password = "";
+
+  for (let i = 0; i < pwdLength; i++) {
+    let generatePwd = Math.floor(Math.random() * character.length);
+    password += character.substring(generatePwd, generatePwd + 1);
+  }
+
+  passwordInput.value = password;
+};
+
+copyButton.onclick = function copyPassword() {
+  const passwordValue = passwordInput.value;
+
+  if (passwordValue.trim() == "") {
+    alert("Password is empty. Generate a password first");
+  } else {
+    navigator.clipboard.writeText(passwordValue).then(function () {
+      show.innerHTML = "Your new Password is:<br>" + passwordValue;
+      show.classList.add("active");
+      setTimeout(() => {
+        show.classList.remove("active");
+      }, 2000);
+      passwordInput.value = "";
+    });
+  }
+};
